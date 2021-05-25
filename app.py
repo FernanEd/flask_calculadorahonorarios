3509f2fc8f37bfa9e7e753823b021fcfe072af93
fernaned <fernandoed.bm@gmail.com>
2021-05-25 16:54:39 -0500

    done

--- app.py
+++ app.py
@@ -9,6 +9,20 @@ def index():
     form = HonorariosForm()
 
     if form.validate_on_submit():
-        return render_template('resultado.html')
+        iva = 0.16
+        r_iva = 0.106666
+        r_isr = 0.10
+        honorarios = form.honorarios.data
+        subtotal = honorarios + honorarios * iva
+        neto = subtotal - honorarios * r_iva - honorarios * r_isr
+        calculos = {
+            'iva' : iva,
+            'r_iva' : r_iva,
+            'r_isr' : r_isr,
+            'honorarios' : honorarios,
+            'subtotal' : subtotal,
+            'neto' : neto
+        }
+        return render_template('resultado.html', calculos=calculos)
 
     return render_template('index.html', title='Calcular honorarios', form=form)
